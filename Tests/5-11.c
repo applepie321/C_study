2c2053b8b3c10a151a881016ccd60fed95d6ecd9
applepie321 <apple00551155@gmail.com>
2023-04-03 22:28:36 +0900

    c quiz

--- Tests/5-11.c
+++ Tests/5-11.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
-#include <string.h>
 
-int main()
-{
-    int a = 0, b = 0;
-    char op[30];
-    
-    
-    printf("수식을 한줄로 띄어쓰기로 입력하세요 : \n");
-    scanf("%s", op);
-    char * token = strtok(op, " ");
-    
-    a = atoi(&op[0]);
-    b = atoi(&op[2]);
-    
-    if(op[1]=='+')
-    {
-        printf("%d + %d", a, b);
-    }
-    else
-        if(op[1]=='-')
-	{
-	printf("%d - %d", a, b);
-    }
-        
-	else
-	    if(op[1]=='*')
-	    {
-	    printf("%d * %d", a, b);
-            else
-		printf("%d / %d", a, b);
-	    }
+int main() {
+    int num1, num2;
+    char op;
 
+    printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+    scanf("%d %c %d", &num1, &op, &num2);
+
+    switch (op) {
+        case '+':
+            printf("%d %c %d = %d 입니다.", num1, op, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d %c %d = %d 입니다.", num1, op, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d %c %d = %d 입니다.", num1, op, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d %c %d = %.4f 입니다.", num1, op, num2, (float)num1 / (float)num2);
+            break;
+        case '%':
+            printf("%d %c %d = %d 입니다.", num1, op, num2, num1 % num2);
+            break;
+        default:
+            printf("연산자를 잘못 입력했습니다.");
+            break;
+    }
+
+    return 0;
 }
